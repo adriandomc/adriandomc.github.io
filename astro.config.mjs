a141619138fcf0e408e754e0d23bef1e5a9c1d54
cosmicdinodog <adriandomc@gmail.com>
2024-08-29 13:18:03 -0600

    Add SSR node.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-site: 'https://cosmicdinodog.github.io',
+  site: 'https://cosmicdinodog.github.io',
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
