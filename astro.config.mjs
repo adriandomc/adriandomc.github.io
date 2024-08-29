2c37364dfe57cc08c75c0b4a34dcc50d4bd3d958
cosmicdinodog <adriandomc@gmail.com>
2024-08-29 13:24:24 -0600

    Revert "Add SSR node.js"
    This reverts commit a141619138fcf0e408e754e0d23bef1e5a9c1d54.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cosmicdinodog.github.io',
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+site: 'https://cosmicdinodog.github.io',
 });
