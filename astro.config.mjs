dbe9e33ee5cbe76059d20ebc864cadffb5768849
cosmicdinodog <adriandomc@gmail.com>
2024-09-07 10:51:04 -0600

    Change site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-site: 'https://cosmicdinodog.github.io',
+site: 'https://adriandomc.github.io',
 });
